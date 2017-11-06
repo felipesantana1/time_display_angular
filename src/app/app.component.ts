db5a14a0b0ae3328bb7cc1696c6c2e8ba90c5622
Felipe <felipeo510.fs@gmail.com>
2017-11-06 13:22:53 -0800

    Time display project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'US Time Zone Display';
+  time;
+  setDateTime(zone){
+    this.time = new Date();
+    if (zone === 'PST'){
+      this.time.setHours(this.time.getHours());
+    }
+    else if (zone === 'MST'){
+      this.time.setHours(this.time.getHours()+1);
+    }
+    else if (zone === 'CST'){
+      this.time.setHours(this.time.getHours()+2);
+    }
+    else if (zone === 'EST'){
+      this.time.setHours(this.time.getHours()+3);
+    }
+  }
+  clear_time(){
+    this.time = null;
+  }
 }
